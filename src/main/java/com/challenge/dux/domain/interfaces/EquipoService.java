19a8ee5ce2a19ae8d8a75e6b4ca8ff3cf42a06ee
Meistt <aguslive7@gmail.com>
2025-05-14 20:25:15 -0300

    agrego estructura de proyecto,clases y comienzo a crear funcionalidades

---
+++ src/main/java/com/challenge/dux/domain/interfaces/EquipoService.java
@@ -0,0 +1,9 @@
+package com.challenge.dux.domain.interfaces;
+
+import com.challenge.dux.domain.model.Equipo;
+
+import java.util.List;
+
+public interface EquipoService {
+    public List<Equipo> getEquipos();
+}
---
+++ src/test/java/com/challenge/dux/DuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.dux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
